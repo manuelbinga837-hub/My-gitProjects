83f6ebcd26e88dc3b74b09ae67b0b28d09d6d5cd
Macoder44 <manuelbinga837@gmail.com>
2026-07-23 20:05:15 +0100

    Update listaDeTarefas.js

--- Listadetarefas/listaDeTarefas.js
+++ Listadetarefas/listaDeTarefas.js
@@ -1,1 +1,69 @@
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [ ];
+let input=document.getElementById("tarefa");
+let btn=document.getElementById("btn");
+let msg=document.getElementById("mensagem");
+let lista=document.getElementById("lista");
 
+const mostrar = () => {
+
+    lista.innerHTML = "";
+
+    tarefas.forEach((tarefa,indice) => {
+
+        let p = document.createElement("p");
+        let div = document.createElement("div");
+        let botao = document.createElement("button");
+
+        p.textContent = tarefa;
+        botao.textContent = " X ";
+
+        div.appendChild(p);
+        div.appendChild(botao);
+        lista.appendChild(div);
+
+        botao.addEventListener("click", () => {
+              botao.addEventListener("click", ()=>{
+
+      tarefas.splice(indice, 1);
+
+      localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
+      mostrar();
+
+});
+              
+
+        });
+
+    }); 
+
+}; 
+
+
+const adicionar = () => {
+
+    if(input.value === ""){
+
+        alert("Digite uma tarefa");
+
+    }else{
+
+        tarefas.push(input.value);
+
+        localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
+        mostrar();
+
+        input.value = "";
+
+    }
+
+};
+
+
+btn.addEventListener("click", () => {
+    adicionar();
+});
+
+
+mostrar();
